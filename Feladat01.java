7175afe5edfa507315337fed673345c056e0ac62
peterrr1 <87699844+peterrr1@users.noreply.github.com>
2022-07-12 19:10:42 +0200

    Add files via upload

---
+++ Feladat01.java
@@ -0,0 +1,32 @@
+
+public class Feladat01 {
+	
+	public static int feladat(String[] szinek) {
+		int hossz = szinek.length;
+		int sum = 0;
+		for (int i = 2; i <= hossz; i++) {
+			if (i == 2)
+				sum += (hossz * (hossz-1)) / i;
+			else {
+				int szaml = hossz;
+				int nev = i;
+				for (int j = hossz-1; j > hossz - i + 1; j--) {
+					szaml *= i;
+				}
+				for (int j = i - 1; j > 1; j--) {
+					nev *= j;
+				}
+				sum += (szaml / nev);
+			}
+		}
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		String[] szinek = {"kek", "piros", "sarga", "loila"};
+		int sum = feladat(szinek);
+		System.out.println(sum);
+	}
+
+}
